8a3099d92302732ca4161102b0f849a85f30f09f
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-07-22 21:51:21 -0600

    Chapter 5 - trusting the process

---
+++ lib/bottles.rb
@@ -0,0 +1,82 @@
+class Bottles
+  def song
+    verses(99, 0)
+  end
+
+  def verses(upper, lower)
+    upper.downto(lower).collect { |i| verse(i) }.join("\n")
+  end
+
+  def verse(number)
+    "#{quantity(number).capitalize} #{container(number)} " +
+      "of beer on the wall, " +
+      "#{quantity(number)} #{container(number)} of beer.\n" +
+      "#{action(number)}, " +
+      "#{quantity(successor(number))} #{container(successor(number))} " +
+      "of beer on the wall.\n"
+  end
+
+  def quantity(number)
+    BottleNumber.new(number).quantity
+  end
+
+  def container(number)
+    BottleNumber.new(number).container
+  end
+
+  def action(number)
+    BottleNumber.new(number).action
+  end
+
+  def successor(number)
+    BottleNumber.new(number).successor
+  end
+end
+
+class BottleNumber
+  attr_accessor :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def quantity()
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
+  end
+
+  def container()
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  def action()
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun} down and pass it around"
+    end
+  end
+
+  def pronoun()
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def successor()
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
+end
